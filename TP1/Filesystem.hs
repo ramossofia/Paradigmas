ff4879281d606bb6ef4bc08299ab75a4264d8cfb
Maria Sarachaga <141249907+mariasarachaga@users.noreply.github.com>
2024-08-21 13:12:54 -0300

    Update Filesystem.hs

--- TP1/Filesystem.hs
+++ TP1/Filesystem.hs
@@ -9,13 +9,23 @@ nuevoF :: FileSystem -- permite obtener un nuevo FileSystem
 nuevoF = FS [] []
 
 departamentosF :: FileSystem -> [Departamento] -- dado un FileSystem retorna los departamentos que incluye
-departamentosF (FS deps) = deps
+departamentosF (FS deps _ ) = deps
 
 anunciosF :: FileSystem -> [Anuncio] -- dado un FileSystem retorna los anuncios que incluye
-anunciosF (FS anuncios) = anuncios
+anunciosF (FS _ anuncios) = anuncios
 
 agregarAnuncioF :: Anuncio -> FileSystem -> FileSystem -- permite agregar un anuncio
+agregarAnuncioF anuncio (FS departamentos anuncios) = FS departamentos (anuncio : anuncios)
+
+
 sacarAnuncioF :: Anuncio -> FileSystem -> FileSystem -- permite eliminar un anuncio
+sacarAnuncioF anuncio ( FS departamentos anuncios) = FS departamentos [a | a <- anuncios, a /= anuncio]
+
+
 agregarDepartamentoF :: Departamento -> FileSystem -> FileSystem -- permite agregar un departamento
+agregarDepartamentoF departamento (FS departamentos anuncios) = FS (departamentos ++ [departamento]) anuncios
+ 
 sacarDepartamentoF :: Departamento -> FileSystem -> FileSystem -- permite eliminar un departamento
+sacarDepartamentoF departamento ( FS departamentos anuncios) =  FS departamentos [a | a <- departamentos, a /= departamento]
+
 anunciosParaF :: [Departamento] -> FileSystem -> [Anuncio] -- entrega los anuncios a emitir para un conjunto de departamentos
