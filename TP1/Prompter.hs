b1a678d6b20c8128622859414700d3094d22943a
Sofia Ramos <141259824+ramossofia@users.noreply.github.com>
2024-08-23 13:42:16 -0300

    Update Prompter.hs

--- TP1/Prompter.hs
+++ TP1/Prompter.hs
@@ -7,10 +7,10 @@ import Tipos
 data Prompter = Pro FileSystem [Departamento] Int deriving (Eq, Show)
 
 nuevoP :: FileSystem -> Prompter -- permite obtener un nuevo Prompter en base a un FileSystem
-nuevoP fs = Pro fs [] 0 
+nuevoP fs = Pro fs [] 0
 
 archivosR :: Prompter -> FileSystem -- dado un prompter retorna su fileSystem
-archivosR (Pro fs _ _ )= fs
+archivosR (Pro fs _ _) = fs
 
 departamentosP :: Prompter -> [Departamento] -- dado un prompter retorna sus departamentos
 departamentosP (Pro _ deps _) = deps
@@ -18,8 +18,11 @@ departamentosP (Pro _ deps _) = deps
 configurarP :: Prompter -> [Departamento] -> Prompter -- Prepara el prompter para emitir los anuncios en los departementos indicados
 configurarP (Pro fs _ index) deps = Pro fs deps index
 
-nunciosP :: Prompter -> [Nombre] -- entrega la lista de nombres de anuncios configurados
+anunciosP :: Prompter -> [Nombre] -- entrega la lista de nombres de anuncios configurados
+anunciosP (Pro fs _ _) = map nombreA (anunciosF fs)
+
 showP :: Prompter -> Anuncio -- muestra el anuncio actual
 avanzarP :: Prompter -> Prompter -- pasa al siguiente anuncio
-duracionP :: Prompter -> Duracion -- indica la duracion total de los anuncios configurados
+avanzarP (Pro fs deps index) = Pro fs deps (index + 1)
 
+duracionP :: Prompter -> Duracion -- indica la duracion total de los anuncios configurados
