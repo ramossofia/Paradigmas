82a00552347b930b479c2280d43d16bf6ab19fc2
Sofia Ramos <141259824+ramossofia@users.noreply.github.com>
2024-08-23 17:04:17 -0300

    Update Prompter.hs

--- TP1/Prompter.hs
+++ TP1/Prompter.hs
@@ -6,6 +6,13 @@ import Tipos
 
 data Prompter = Pro FileSystem [Departamento] Int deriving (Eq, Show)
 
+-- Funciones auxiliares:
+obtenerAnuncios :: Prompter -> [Anuncio] -- permite obtener la lista de anuncios desde el Filesystem
+obtenerAnuncios (Pro fs _ _) = anunciosF fs
+
+anunciosConfigurados :: Prompter -> [Anuncio] -- permite obtener la lista de anuncios configurados
+anunciosConfigurados prompter = anunciosParaF (departamentosP prompter) (archivosR prompter)
+
 nuevoP :: FileSystem -> Prompter -- permite obtener un nuevo Prompter en base a un FileSystem
 nuevoP fs = Pro fs [] 0
 
@@ -19,13 +26,29 @@ configurarP :: Prompter -> [Departamento] -> Prompter -- Prepara el prompter par
 configurarP (Pro fs _ index) deps = Pro fs deps index
 
 anunciosP :: Prompter -> [Nombre] -- entrega la lista de nombres de anuncios configurados
-anunciosP (Pro fs _ _) = map nombreA (anunciosF fs)
+anunciosP prompter = map nombreA (anunciosConfigurados prompter)
 
 showP :: Prompter -> Anuncio -- muestra el anuncio actual
-showP (Pro fs _ index) = (anunciosF fs) !! (index `mod` length (anunciosF fs))
+showP prompter = (anunciosConfigurados prompter) !! (index `mod` length anuncios)
+  where
+    anuncios = anunciosConfigurados prompter
+    index = case anuncios of
+      [] -> 0
+      _ -> currentIndex prompter
+    currentIndex (Pro _ _ idx) = idx
 
 avanzarP :: Prompter -> Prompter -- pasa al siguiente anuncio
-avanzarP (Pro fs deps index) = Pro fs deps ((index + 1) `mod` length (anunciosF fs))
+avanzarP prompter = Pro fs deps nextIndex
+  where
+    fs = archivosR prompter
+    deps = departamentosP prompter
+    anuncios = anunciosConfigurados prompter
+    nextIndex = (currentIndex + 1) `mod` length anuncios
+    currentIndex = case anuncios of
+      [] -> 0
+      _ -> index
+    index = case prompter of
+      Pro _ _ idx -> idx
 
 duracionP :: Prompter -> Duracion -- indica la duracion total de los anuncios configurados
 duracionP (Pro fs deps _) = foldr ((+) . duracionA) 0 (anunciosParaF deps fs)
