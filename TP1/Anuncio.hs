9bbfeb591d6f37b372a625a7ca36323919d3a5a6
Maria Sarachaga <141249907+mariasarachaga@users.noreply.github.com>
2024-08-22 21:05:48 -0300

    Update Anuncio.hs

--- TP1/Anuncio.hs
+++ TP1/Anuncio.hs
@@ -1,26 +1,34 @@
-module Anuncio (Anuncio, nuevoA, nombreA, duracionA, departamentosA, agreagarA, sacarA, aplicaA) where
+module Anuncio (Anuncio, nuevoA, nombreA, duracionA, departamentosA, agregarA, sacarA, aplicaA) where
 
-import Tipos
+import qualified Tipos as T 
 
-data Anuncio = Anu Nombre [Departamento] Duracion deriving (Eq, Show, Ord)
+data Anuncio = Anu T.Nombre [T.Departamento] T.Duracion deriving (Eq, Show, Ord)
 
-nuevoA :: Nombre -> Duracion -> Anuncio -- dado un nombre y una duracion en segundos retorna un nuevo Anuncio
-nuevoA nombre duracion = Anu Nombre [] duracion
 
-nombreA :: Anuncio -> Nombre -- dado un anuncio retorna su nombre
+nuevoA :: T.Nombre -> T.Duracion -> Anuncio -- dado un nombre y una duracion en segundos retorna un nuevo Anuncio
+nuevoA nombre duracion = Anu nombre [] duracion
+
+
+nombreA :: Anuncio -> T.Nombre -- dado un anuncio retorna su nombre
 nombreA (Anu nombre _ _) = nombre
 
-duracionA :: Anuncio -> Duracion -- dado un anuncio retorna su duracion
+
+duracionA :: Anuncio -> T.Duracion -- dado un anuncio retorna su duracion
 duracionA (Anu _ _ duracion) = duracion
 
-departamentosA :: Anuncio -> [Departamento] -- dado un anuncio retorna los departamentos que le fueron asociados
+
+departamentosA :: Anuncio -> [T.Departamento] -- dado un anuncio retorna los departamentos que le fueron asociados
 departamentosA (Anu _ departamentos _) = departamentos
 
-agregarA :: Departamento -> Anuncio -> Anuncio -- permite asignar un departamento a un anuncio
+
+agregarA :: T.Departamento -> Anuncio -> Anuncio -- permite asignar un departamento a un anuncio
 agregarA departamento (Anu nombre departamentos duracion) = Anu nombre (departamentos ++ [departamento]) duracion
 
-sacarA :: Departamento -> Anuncio -> Anuncio -- permite quitarle un departamento a un anuncio
+
+sacarA :: T.Departamento -> Anuncio -> Anuncio -- permite quitarle un departamento a un anuncio
+sacarA departamento (Anu nombre departamentos duracion) = Anu nombre (filter (/= departamento) departamentos) duracion
 
 
-aplicaA :: [Departamento] -> Anuncio -> Bool -- responde si un anuncion debe emitirse para alguno de los departamentos consultados
+aplicaA :: [T.Departamento] -> Anuncio -> Bool -- responde si un anuncio debe emitirse para alguno de los departamentos consultados
 aplicaA deptos (Anu _ departamentos _) = any (`elem` departamentos) deptos
+
