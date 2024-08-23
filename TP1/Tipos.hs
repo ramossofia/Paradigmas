efd580b0a3c27d758a005a0877e7a982bcd446e2
Sofia Ramos <141259824+ramossofia@users.noreply.github.com>
2024-08-23 13:00:05 -0300

    Update Tipos.hs

--- TP1/Tipos.hs
+++ TP1/Tipos.hs
@@ -5,9 +5,3 @@ type Duracion = Int
 type Departamento = String
 
 type Nombre = String
-
-type Anuncio = (Nombre, [Departamento], Duracion)
-
-type FileSystem = ([Departamento], [Anuncio])
-
-type Prompter = ([Departamento], [Anuncio], [Departamento], Int)
