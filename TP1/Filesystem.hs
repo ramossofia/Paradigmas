5c9cebc341172a7f5bf1434eaa022e23f02f1276
Sofia Ramos <141259824+ramossofia@users.noreply.github.com>
2024-08-23 13:26:14 -0300

    Update Filesystem.hs

--- TP1/Filesystem.hs
+++ TP1/Filesystem.hs
@@ -9,7 +9,7 @@ nuevoF :: FileSystem -- permite obtener un nuevo FileSystem
 nuevoF = FS [] []
 
 departamentosF :: FileSystem -> [Departamento] -- dado un FileSystem retorna los departamentos que incluye
-departamentosF (FS deps _ ) = deps
+departamentosF (FS deps _) = deps
 
 anunciosF :: FileSystem -> [Anuncio] -- dado un FileSystem retorna los anuncios que incluye
 anunciosF (FS _ anuncios) = anuncios
@@ -17,15 +17,14 @@ anunciosF (FS _ anuncios) = anuncios
 agregarAnuncioF :: Anuncio -> FileSystem -> FileSystem -- permite agregar un anuncio
 agregarAnuncioF anuncio (FS departamentos anuncios) = FS departamentos (anuncio : anuncios)
 
-
 sacarAnuncioF :: Anuncio -> FileSystem -> FileSystem -- permite eliminar un anuncio
-sacarAnuncioF anuncio ( FS departamentos anuncios) = FS departamentos [a | a <- anuncios, a /= anuncio]
-
+sacarAnuncioF anuncio (FS departamentos anuncios) = FS departamentos [a | a <- anuncios, a /= anuncio]
 
 agregarDepartamentoF :: Departamento -> FileSystem -> FileSystem -- permite agregar un departamento
-agregarDepartamentoF departamento (FS departamentos anuncios) = FS (departamentos ++ [departamento]) anuncios
- 
+agregarDepartamentoF departamento (FS departamentos anuncios) = FS (departamento : departamentos) anuncios
+
 sacarDepartamentoF :: Departamento -> FileSystem -> FileSystem -- permite eliminar un departamento
-sacarDepartamentoF departamento ( FS departamentos anuncios) =  FS departamentos [a | a <- departamentos, a /= departamento]
+sacarDepartamentoF departamento (FS departamentos anuncios) = FS (filter (/= departamento) departamentos) anuncios
 
 anunciosParaF :: [Departamento] -> FileSystem -> [Anuncio] -- entrega los anuncios a emitir para un conjunto de departamentos
+anunciosParaF departamentos (FS _ anuncios) = filter (aplicaA departamentos) anuncios
