34e2ac9c25eac609e315c76517e8d00e9656b2c8
Maria Sarachaga <141249907+mariasarachaga@users.noreply.github.com>
2024-08-27 12:24:46 -0300

    Update Filesystem.hs

--- TP1/Filesystem.hs
+++ TP1/Filesystem.hs
@@ -5,26 +5,35 @@ import Tipos
 
 data FileSystem = FS [Departamento] [Anuncio] deriving (Eq, Show)
 
-nuevoF :: FileSystem -- permite obtener un nuevo FileSystem
+nuevoF :: FileSystem
 nuevoF = FS [] []
 
-departamentosF :: FileSystem -> [Departamento] -- dado un FileSystem retorna los departamentos que incluye
+departamentosF :: FileSystem -> [Departamento]
 departamentosF (FS deps _) = deps
+--si no hay departamentos que devuelva una lista vacia
 
-anunciosF :: FileSystem -> [Anuncio] -- dado un FileSystem retorna los anuncios que incluye
+
+anunciosF :: FileSystem -> [Anuncio]
 anunciosF (FS _ anuncios) = anuncios
+--si no hay anuncios que devuelva una lista vacia
 
-agregarAnuncioF :: Anuncio -> FileSystem -> FileSystem -- permite agregar un anuncio
+
+agregarAnuncioF :: Anuncio -> FileSystem -> FileSystem
 agregarAnuncioF anuncio (FS departamentos anuncios) = FS departamentos (anuncio : anuncios)
+--si ya existe el anuncio printear que el anuncio se encuentra en sistema y no agregar nada.
 
-sacarAnuncioF :: Anuncio -> FileSystem -> FileSystem -- permite eliminar un anuncio
+sacarAnuncioF :: Anuncio -> FileSystem -> FileSystem
 sacarAnuncioF anuncio (FS departamentos anuncios) = FS departamentos (filter (/= anuncio) anuncios)
+-- si no se encuentra el anuncio  en el sistema de archivos printear que no se encuentra ese archivo.
 
-agregarDepartamentoF :: Departamento -> FileSystem -> FileSystem -- permite agregar un departamento
+agregarDepartamentoF :: Departamento -> FileSystem -> FileSystem
 agregarDepartamentoF departamento (FS departamentos anuncios) = FS (departamento : departamentos) anuncios
+--si ya existe el departamento en el sistema de archivos que no lo agregue de nuevo y printee que ya existe .
 
-sacarDepartamentoF :: Departamento -> FileSystem -> FileSystem -- permite eliminar un departamento
+sacarDepartamentoF :: Departamento -> FileSystem -> FileSystem
 sacarDepartamentoF departamento (FS departamentos anuncios) = FS (filter (/= departamento) departamentos) anuncios
+--si nos e encuentra el departamento en el sistema de archivos printear que no se encuentra.
 
-anunciosParaF :: [Departamento] -> FileSystem -> [Anuncio] -- entrega los anuncios a emitir para un conjunto de departamentos
-anunciosParaF departamentos (FS _ anuncios) = filter (aplicaA departamentos) anuncios
+anunciosParaF :: [Departamento] -> FileSystem -> [Anuncio]
+anunciosParaF departamentos (FS _ anuncios) = filter (\a -> aplicaA departamentos a) anuncios
+--si9 se ingresa un departamento que no existe, devolver vacio.
