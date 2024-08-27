ee91ca75a238912aca077bda2f9342c666edc285
Sofia Ramos <141259824+ramossofia@users.noreply.github.com>
2024-08-27 12:45:22 -0300

    Update Tipos.hs

--- TP1/Tipos.hs
+++ TP1/Tipos.hs
@@ -5,3 +5,7 @@ type Duracion = Int
 type Departamento = String
 
 type Nombre = String
+
+verificarVacio :: String -> String -> String
+verificarVacio "" _ = error "El campo no puede estar vacio"
+verificarVacio valor _ = valor
