743717dd61ecc4632cadc38be8e5c9c4e06a1a00
Maria Sarachaga <141249907+mariasarachaga@users.noreply.github.com>
2024-08-29 19:35:24 -0300

    Update Tests.hs

--- TP1/Tests.hs
+++ TP1/Tests.hs
@@ -1,21 +1,11 @@
 module Tests where
 
 import Anuncio
-import Control.Exception
-import FileSystem
+import FileSystem (FileSystem, nuevoF, agregarAnuncioF, sacarAnuncioF, agregarDepartamentoF, sacarDepartamentoF, anunciosF, departamentosF, anunciosParaF, buscarAnuncio)
 import Prompter (Prompter, nuevoP, archivosR, departamentosP, configurarP, anunciosP, showP, avanzarP, duracionP)
+import Control.Exception
 import System.IO.Unsafe
 import Tipos
-
--- Datos de ejemplo para pruebas
-anuncio1 :: Anuncio
-anuncio1 = nuevoA "Anuncio1" 30
-
-anuncio2 :: Anuncio
-anuncio2 = nuevoA "Anuncio2" 45
-
-fileSystemEjemplo :: FileSystem
-fileSystemEjemplo = agregarAnuncioF anuncio1 . agregarAnuncioF anuncio2 $ nuevoF
 
 -- Función para probar excepciones
 testF :: (Show a) => a -> Bool
@@ -28,49 +18,116 @@ testF action = unsafePerformIO $ do
     isException :: SomeException -> Maybe ()
     isException _ = Just ()
 
--- Funciones de prueba para FileSystem
+-- Pruebas para el módulo Anuncio (Excepciones)
+testNuevoAConDuracionNegativa :: Bool
+testNuevoAConDuracionNegativa = testF (nuevoA "Test" (-5))
 
+testNombreAConNombreVacio :: Bool
+testNombreAConNombreVacio =
+  let anuncio = nuevoA "" 60
+  in testF (nombreA anuncio)
+
+testDuracionAConDuracionInvalida :: Bool
+testDuracionAConDuracionInvalida =
+  let anuncio = nuevoA "Test" 0
+  in testF (duracionA anuncio)
+
+testDepartamentosASinDepartamentos :: Bool
+testDepartamentosASinDepartamentos = testF (departamentosA (nuevoA "Test" 60))
+
+testAgregarAConDepartamentoRepetido :: Bool
+testAgregarAConDepartamentoRepetido =
+  let anuncio = agregarA "Dept1" (nuevoA "Test" 60)
+  in testF (agregarA "Dept1" anuncio)
+
+testSacarADeAnuncioSinDepartamentos :: Bool
+testSacarADeAnuncioSinDepartamentos = testF (sacarA "Dept1" (nuevoA "Test" 60))
+
+testAplicaAConDepartamentosVacios :: Bool
+testAplicaAConDepartamentosVacios = testF (aplicaA [] (nuevoA "Test" 60))
+
+-- Pruebas para el módulo FileSystem (Excepciones)
+testAgregarAnuncioFRepetido :: Bool
+testAgregarAnuncioFRepetido =
+  let fs = agregarAnuncioF (nuevoA "Test" 60) nuevoF
+  in testF (agregarAnuncioF (nuevoA "Test" 60) fs)
+
+testSacarAnuncioFNoExistente :: Bool
+testSacarAnuncioFNoExistente = testF (sacarAnuncioF (nuevoA "Test" 60) nuevoF)
+
+testAgregarDepartamentoFRepetido :: Bool
+testAgregarDepartamentoFRepetido =
+  let fs = agregarDepartamentoF "Dept1" nuevoF
+  in testF (agregarDepartamentoF "Dept1" fs)
+
+testSacarDepartamentoFNoExistente :: Bool
+testSacarDepartamentoFNoExistente = testF (sacarDepartamentoF "Dept1" nuevoF)
+
+testAnunciosParaFConDepartamentosVacios :: Bool
+testAnunciosParaFConDepartamentosVacios = testF (anunciosParaF [] nuevoF)
+
+-- Funciones de prueba para Prompter (Excepciones)
+testShowP :: Bool
+testShowP =
+  testF (showP prompterConfigurado)
+  where
+    prompter = nuevoP fileSystemEjemplo
+    prompterConfigurado = configurarP prompter []
+
+testAvanzarP :: Bool
+testAvanzarP =
+  testF (nombreA (showP prompterAvanzado) == "Anuncio2")
+  where
+    prompter = nuevoP fileSystemEjemplo
+    prompterConfigurado = configurarP prompter ["Dept1"]
+    prompterAvanzado = avanzarP prompterConfigurado
+
+-- Pruebas de funcionamiento normal (sin excepciones)
+
+-- Ejemplo de datos para pruebas
+anuncio1 :: Anuncio
+anuncio1 = nuevoA "Anuncio1" 30
+
+anuncio2 :: Anuncio
+anuncio2 = nuevoA "Anuncio2" 45
+
+fileSystemEjemplo :: FileSystem
+fileSystemEjemplo = agregarAnuncioF anuncio1 . agregarAnuncioF anuncio2 $ nuevoF
+
+-- Funciones de prueba para FileSystem (funcionamiento normal)
 testNuevoF :: Bool
-testNuevoF =
-  departamentosF fs == [] && anunciosF fs == []
+testNuevoF = departamentosF fs == [] && anunciosF fs == []
   where
     fs = nuevoF
 
 testAgregarAnuncioF :: Bool
-testAgregarAnuncioF =
-  anunciosF fs == [anuncio1]
+testAgregarAnuncioF = anunciosF fs == [anuncio1]
   where
     fs = agregarAnuncioF anuncio1 nuevoF
 
 testSacarAnuncioF :: Bool
-testSacarAnuncioF =
-  anunciosF fs == [anuncio2]
+testSacarAnuncioF = anunciosF fs == [anuncio2]
   where
     fs = sacarAnuncioF anuncio1 fileSystemEjemplo
 
 testAgregarDepartamentoF :: Bool
-testAgregarDepartamentoF =
-  departamentosF fs == ["Dept1"]
+testAgregarDepartamentoF = departamentosF fs == ["Dept1"]
   where
     fs = agregarDepartamentoF "Dept1" nuevoF
 
 testSacarDepartamentoF :: Bool
-testSacarDepartamentoF =
-  departamentosF fs == []
+testSacarDepartamentoF = departamentosF fs == []
   where
     fs = sacarDepartamentoF "Dept1" (agregarDepartamentoF "Dept1" nuevoF)
 
 testAnunciosParaF :: Bool
-testAnunciosParaF =
-  anuncios == [anuncio1, anuncio2]
+testAnunciosParaF = anuncios == [anuncio1, anuncio2]
   where
     fs = fileSystemEjemplo
     departamentos = ["Dept1"]
     anuncios = anunciosParaF departamentos fs
 
-
--- Funciones de prueba para Anuncio
-
+-- Funciones de prueba para Anuncio (funcionamiento normal)
 testNuevoA :: Bool
 testNuevoA =
   nombreA anuncio == "AnuncioTest" && duracionA anuncio == 60
@@ -83,12 +140,6 @@ testAgregarA =
   where
     anuncio = agregarA "Dept1" anuncio1
 
-testSacarA :: Bool
-testSacarA =
-  departamentosA anuncio == []
-  where
-    anuncio = sacarA "Dept1" (agregarA "Dept1" anuncio1)
-
 testAplicaA :: Bool
 testAplicaA =
   aplicaA departamentos anuncioAplicable
@@ -97,92 +148,83 @@ testAplicaA =
     departamentos = ["Dept1"]
     anuncioAplicable = agregarA "Dept1" anuncio
 
--- Funciones de prueba para Prompter
-
-testNuevoP :: Bool
-testNuevoP =
-  archivosIguales && departamentosIguales && indiceIgual
-  where
-    prompter = nuevoP fileSystemEjemplo
-    archivosIguales = archivosR prompter == fileSystemEjemplo
-    departamentosIguales = departamentosP prompter == []
-    indiceIgual = currentIndex prompter == 0
-
-    currentIndex :: Prompter -> Int
-    currentIndex p = case showP p of
-      "Anuncio1" -> 0
-      _ -> 1
-
-
+-- Funciones de prueba para Prompter (funcionamiento normal)
 testConfigurarP :: Bool
-testConfigurarP =
-  departamentosPResult == ["Dept1", "Dept2"]
+testConfigurarP = departamentosPResult == ["Dept1", "Dept2"]
   where
-    prompter = nuevoP fileSystemEjemplo
-    -- Configura el Prompter con departamentos
+    fsConDept1Dept2 = agregarDepartamentoF "Dept1" $ agregarDepartamentoF "Dept2" nuevoF
+    prompter = nuevoP fsConDept1Dept2
     prompterConAnuncios = configurarP prompter ["Dept1", "Dept2"]
-    departamentosPResult = departamentosP (configurarP prompterConAnuncios ["Dept1", "Dept2"])
+    departamentosPResult = departamentosP prompterConAnuncios
+
+
 
 testAnunciosP :: Bool
-testAnunciosP =
-  anunciosPResult == ["Anuncio1", "Anuncio2"]
+testAnunciosP = anunciosPResult == ["Anuncio1", "Anuncio2"]
   where
-    prompter = nuevoP fileSystemEjemplo
-    -- Configura el Prompter sin anuncios
-    prompterConAnuncios = configurarP prompter []
-    anunciosPResult = anunciosP (configurarP prompterConAnuncios ["Dept1"])
-
--- Prueba para showP
-testShowP :: Bool
-testShowP =
-  showP prompterConfigurado == "No hay anuncios configurados"
-  where
-    prompter = nuevoP fileSystemEjemplo
-    prompterConfigurado = configurarP prompter []
-
--- Prueba para avanzarP
-testAvanzarP :: Bool
-testAvanzarP =
-  showP prompterAvanzado == "Anuncio2"
-  where
-    prompter = nuevoP fileSystemEjemplo
-    prompterConfigurado = configurarP prompter ["Dept1"]
-    prompterAvanzado = avanzarP prompterConfigurado
+    fsConDept1 = agregarDepartamentoF "Dept1" $ nuevoF
+    fsConAnunciosYDeptos = agregarAnuncioF anuncio1 $ agregarAnuncioF anuncio2 fsConDept1
+    prompter = nuevoP fsConAnunciosYDeptos
+    prompterConAnuncios = configurarP prompter ["Dept1"]
+    anunciosPResult = anunciosP prompterConAnuncios
 
 testDuracionP :: Bool
-testDuracionP =
-  duracionP prompterConfigurado == 75
+testDuracionP = duracionP prompterConfigurado == 75
   where
-    prompter = nuevoP fileSystemEjemplo
+    -- Inicializa el FileSystem con un departamento y dos anuncios
+    fsConDept1 = agregarDepartamentoF "Dept1" $ nuevoF
+    fsConAnunciosYDeptos = agregarAnuncioF anuncio1 $ agregarAnuncioF anuncio2 fsConDept1
+    -- Inicializa el Prompter con el FileSystem configurado
+    prompter = nuevoP fsConAnunciosYDeptos
+    -- Configura el Prompter con el departamento y obtiene los anuncios configurados
     prompterConfigurado = configurarP prompter ["Dept1"]
+    -- Obtiene los nombres de los anuncios configurados
+    nombresAnuncios = anunciosP prompterConfigurado
+    -- Busca los anuncios en el FileSystem
+    anunciosEncontrados = map (\nombre -> buscarAnuncio nombre fsConAnunciosYDeptos) nombresAnuncios
+    -- Calcula la duración esperada
+    duracionEsperada = sum (map duracionA anunciosEncontrados)
+
 
 -- Función para ejecutar todas las pruebas e imprimir los resultados
 runTests :: IO ()
 runTests = do
-  let results = [ ("testNuevoF", testNuevoF)
-                 , ("testAgregarAnuncioF", testAgregarAnuncioF)
-                 , ("testSacarAnuncioF", testSacarAnuncioF)
-                 , ("testAgregarDepartamentoF", testAgregarDepartamentoF)
-                 , ("testSacarDepartamentoF", testSacarDepartamentoF)
-                 , ("testAnunciosParaF", testAnunciosParaF)
-                 , ("testNuevoA", testNuevoA)
-                 , ("testAgregarA", testAgregarA)
-                 , ("testSacarA", testSacarA)
-                 , ("testAplicaA", testAplicaA)
-                 , ("testNuevoP", testNuevoP)
-                 , ("testConfigurarP", testConfigurarP)
-                 , ("testAnunciosP", testAnunciosP)
-                 , ("testShowP", testShowP)
-                 , ("testAvanzarP", testAvanzarP)
-                 , ("testDuracionP", testDuracionP)
-                 ]
-  let allPassed = all snd results
-  mapM_ printResult results
-  if allPassed
-    then putStrLn "¡Todo OK!"
-    else putStrLn "Algunas pruebas han fallado."
+  putStrLn "\n#####################################"
+  putStrLn "\nEjecutando pruebas de excepciones..."
 
-printResult :: (String, Bool) -> IO ()
-printResult (name, result) =
-  putStrLn $ name ++ ": " ++ if result then "Passed" else "Failed"
+  let exceptionResults = [ ("\ntestNuevoAConDuracionNegativa", testNuevoAConDuracionNegativa)
+                         , ("testNombreAConNombreVacio", testNombreAConNombreVacio)
+                         , ("testDuracionAConDuracionInvalida", testDuracionAConDuracionInvalida)
+                         , ("testDepartamentosASinDepartamentos", testDepartamentosASinDepartamentos)
+                         , ("testAgregarAConDepartamentoRepetido", testAgregarAConDepartamentoRepetido)
+                         , ("testSacarADeAnuncioSinDepartamentos", testSacarADeAnuncioSinDepartamentos)
+                         , ("testAplicaAConDepartamentosVacios", testAplicaAConDepartamentosVacios)
+                         , ("testAgregarAnuncioFRepetido", testAgregarAnuncioFRepetido)
+                         , ("testSacarAnuncioFNoExistente", testSacarAnuncioFNoExistente)
+                         , ("testAgregarDepartamentoFRepetido", testAgregarDepartamentoFRepetido)
+                         , ("testSacarDepartamentoFNoExistente", testSacarDepartamentoFNoExistente)
+                         , ("testAnunciosParaFConDepartamentosVacios", testAnunciosParaFConDepartamentosVacios)
+                         , ("testShowP", testShowP)
+                         , ("testAvanzarP", testAvanzarP)
+                         ]
+  mapM_ printResult exceptionResults
 
+  putStrLn "\n##############################################"
+  putStrLn "\nEjecutando pruebas de funcionamiento normal..."
+  let normalResults = [ ("testNuevoF", testNuevoF)
+                      , ("testAgregarAnuncioF", testAgregarAnuncioF)
+                      , ("testSacarAnuncioF", testSacarAnuncioF)
+                      , ("testAgregarDepartamentoF", testAgregarDepartamentoF)
+                      , ("testSacarDepartamentoF", testSacarDepartamentoF)
+                      , ("testAnunciosParaF", testAnunciosParaF)
+                      , ("testNuevoA", testNuevoA)
+                      , ("testAgregarA", testAgregarA)
+                      , ("testAplicaA", testAplicaA)
+                      , ("testConfigurarP", testConfigurarP)
+                      , ("testAnunciosP", testAnunciosP)
+                      , ("testDuracionP", testDuracionP)
+                      ]
+  mapM_ printResult normalResults
+
+  where
+    printResult (testName, result) = putStrLn $ testName ++ ": " ++ (if result then "Passed" else "Failed")
