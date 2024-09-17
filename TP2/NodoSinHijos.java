82dba72b27afefe293eb06cfcc8e14d1b3060875
Maria Sarachaga <141249907+mariasarachaga@users.noreply.github.com>
2024-09-17 11:03:17 -0300

    Update NodoSinHijos.java

--- TP2/NodoSinHijos.java
+++ TP2/NodoSinHijos.java
@@ -9,32 +9,29 @@ public class NodoSinHijos extends Nodo {
         super(carga);
     }
 
-    @Override
     public Nodo atLeft(Nodo left) {
         return new NodoConHijoIzquierdo(carga, left);
     }
 
-    @Override
     public Nodo atRight(Nodo right) {
         return new NodoConHijoDerecho(carga, right);
     }
 
-    @Override
     public Nodo left() {
         throw new UnsupportedOperationException("Nada a la siniestra!");
     }
 
-    @Override
+
     public Nodo right() {
         throw new UnsupportedOperationException("Nada a la diestra!");
     }
 
-    @Override
+
     public void dfs(LinkedList<Object> result) {
         result.add(carga);
     }
 
-    @Override
+
     public void addChildrenToQueue(Queue<Nodo> queue) {
     }
 }
