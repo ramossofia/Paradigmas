b18dc1c19018bf38ae83cb2ed51d0ab5ed00d571
Maria Sarachaga <141249907+mariasarachaga@users.noreply.github.com>
2024-09-17 11:00:13 -0300

    Update NodoConHijos.java

--- TP2/NodoConHijos.java
+++ TP2/NodoConHijos.java
@@ -13,34 +13,33 @@ public class NodoConHijos extends Nodo {
         this.right = right;
     }
 
-    @Override
+
     public Nodo atLeft(Nodo left) {
         return new NodoConHijos(carga, left, this.right);
     }
 
-    @Override
+
     public Nodo atRight(Nodo right) {
         return new NodoConHijos(carga, this.left, right);
     }
 
-    @Override
     public Nodo left() {
         return left;
     }
 
-    @Override
+
     public Nodo right() {
         return right;
     }
 
-    @Override
+
     public void dfs(LinkedList<Object> result) {
         result.add(carga);
         left.dfs(result);
         right.dfs(result);
     }
 
-    @Override
+
     public void addChildrenToQueue(Queue<Nodo> queue) {
         queue.add(left);
         queue.add(right);
