f996b0e3201b71ab0ec45aee80c5bb9a60b388c5
Maria Sarachaga <141249907+mariasarachaga@users.noreply.github.com>
2024-09-18 19:32:22 -0300

    Update NodoConHijoDerecho.java

--- TP2/NodoConHijoDerecho.java
+++ TP2/NodoConHijoDerecho.java
@@ -3,40 +3,59 @@ package tree;
 import java.util.LinkedList;
 import java.util.Queue;
 
-public class NodoConHijoDerecho extends Nodo {
-    private Nodo right;
+public class AristaExistente extends Arista {
+    private Object carga;
+    private Arista left;
+    private Arista right;
 
-    public NodoConHijoDerecho(Object carga, Nodo right) {
-        super(carga);
-        this.right = right;
+    public AristaExistente(Object carga) {
+        this.carga = carga;
+        this.left = new AristaNula("Nada a la siniestra!");
+        this.right = new AristaNula("Nada a la diestra!");
     }
 
-    public Nodo atLeft(Nodo left) {
-        return new NodoConHijos(carga, left, this.right);
-    }
-
-
-    public Nodo atRight(Nodo right) {
-        return new NodoConHijos(carga, null, right);
-    }
-
-    public Nodo left() {
-        throw new UnsupportedOperationException("Nada a la siniestra!");
-    }
-
-
-    public Nodo right() {
-        return right;
-    }
-
-
+    @Override
     public void dfs(LinkedList<Object> result) {
         result.add(carga);
+        left.dfs(result);
         right.dfs(result);
     }
 
+    @Override
+    public void addChildrenToQueue(Queue<Arista> queue) {
+        left.addToQueueIfNotNull(queue);
+        right.addToQueueIfNotNull(queue);
+    }
 
-    public void addChildrenToQueue(Queue<Nodo> queue) {
-        queue.add(right);
+    @Override
+    public Object getCarga() {
+        return carga;
+    }
+
+    @Override
+    public Arista atLeft(Arista left) {
+        this.left = left;
+        return this;
+    }
+
+    @Override
+    public Arista atRight(Arista right) {
+        this.right = right;
+        return this;
+    }
+
+    @Override
+    public Arista left() {
+        return left;
+    }
+
+    @Override
+    public Arista right() {
+        return right;
+    }
+
+    @Override
+    protected void addToQueueIfNotNull(Queue<Arista> queue) {
+        queue.add(this);
     }
 }
