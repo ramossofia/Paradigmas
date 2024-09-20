aa715a24c0eb64317403916351a3d4727691f234
Sofia Ramos <141259824+ramossofia@users.noreply.github.com>
2024-09-20 17:07:57 -0300

    Update and rename AristaExistente.java to LeafWithSubTree.java

---
+++ TP2/LeafWithSubTree.java
@@ -0,0 +1,53 @@
+package tree;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Queue;
+
+public class LeafWithSubTree extends Leaf {
+    private Object carga;
+    private List<Leaf> children;
+
+    public LeafWithSubTree(Object carga) {
+        this.carga = carga;
+        this.children = new ArrayList<>();
+        this.children.add(new NullLeaf("Nada a la siniestra!"));
+        this.children.add(new NullLeaf("Nada a la diestra!"));
+    }
+
+    @Override
+    public void dfs(ArrayList<Object> result) {
+        result.add(carga);
+        for (Leaf child : children) {
+            child.dfs(result);
+        }
+    }
+
+    @Override
+    public void addChildrenToQueue(Queue<Leaf> queue) {
+        for (Leaf child : children) {
+            child.addToQueueIfNotNull(queue);
+        }
+    }
+
+    @Override
+    public Object getCarga() {
+        return carga;
+    }
+
+    @Override
+    public Leaf addChildAt(int index, Leaf child) {
+        children.set(index, child);
+        return this;
+    }
+
+    @Override
+    public Leaf getChildAt(int index) {
+        return children.get(index);
+    }
+
+    @Override
+    protected void addToQueueIfNotNull(Queue<Leaf> queue) {
+        queue.add(this);
+    }
+}
