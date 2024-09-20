fec5ef4c2155233ac1b6e42c5c6143198d658797
Sofia Ramos <141259824+ramossofia@users.noreply.github.com>
2024-09-20 17:09:27 -0300

    Update Tree.java

--- TP2/Tree.java
+++ TP2/Tree.java
@@ -1,28 +1,29 @@
 package tree;
 
+import java.util.ArrayList;
 import java.util.LinkedList;
 import java.util.Queue;
 
 public class Tree {
-    private Arista root;
+    private Leaf root;
 
     public Tree(Object carga) {
-        this.root = new AristaExistente(carga);
+        this.root = new LeafWithSubTree(carga);
     }
 
-    public LinkedList<Object> dfs() {
-        LinkedList<Object> result = new LinkedList<>();
+    public ArrayList<Object> dfs() {
+        ArrayList<Object> result = new ArrayList<>();
         root.dfs(result);
         return result;
     }
 
-    public LinkedList<Object> bfs() {
-        LinkedList<Object> result = new LinkedList<>();
-        Queue<Arista> queue = new LinkedList<>();
+    public ArrayList<Object> bfs() {
+        ArrayList<Object> result = new ArrayList<>();
+        Queue<Leaf> queue = new LinkedList<>();
         queue.add(root);
 
         while (!queue.isEmpty()) {
-            Arista current = queue.poll();
+            Leaf current = queue.poll();
             result.add(current.getCarga());
             current.addChildrenToQueue(queue);
         }
@@ -31,22 +32,22 @@ public class Tree {
     }
 
     public Tree atLeft(Tree leftTree) {
-        root = root.atLeft(leftTree.root);
+        root = root.addChildAt(0, leftTree.root);
         return this;
     }
 
     public Tree atRight(Tree rightTree) {
-        root = root.atRight(rightTree.root);
+        root = root.addChildAt(1, rightTree.root);
         return this;
     }
 
     public Tree left() {
-        Arista leftChild = root.left();
+        Leaf leftChild = root.getChildAt(0);
         return new Tree(leftChild.getCarga());
     }
 
     public Tree right() {
-        Arista rightChild = root.right();
+        Leaf rightChild = root.getChildAt(1);
         return new Tree(rightChild.getCarga());
     }
 
