5144dec03dad526227101ad167e252c857449319
Sofia Ramos <141259824+ramossofia@users.noreply.github.com>
2024-10-25 14:09:03 -0300

    Update Norte.java

--- TP3/Norte.java
+++ TP3/Norte.java
@@ -1,29 +1,28 @@
 package Explorer;
 
 public class Norte extends Direccion {
-
     @Override
     public void moverAdelante(Explorer explorer) {
-        explorer.actualizarPosicion(explorer.getX(), explorer.getY() + 1); // Mover hacia arriba en el eje Y
+        explorer.actualizarPosicion(explorer.getX(), explorer.getY() + 1);
     }
 
     @Override
     public void moverAtras(Explorer explorer) {
-        explorer.actualizarPosicion(explorer.getX(), explorer.getY() - 1); // Mover hacia abajo en el eje Y
+        explorer.actualizarPosicion(explorer.getX(), explorer.getY() - 1);
     }
 
     @Override
     public Direccion rotarIzquierda() {
-        return new Oeste(); // Rotar 90 grados a la izquierda desde el Norte da como resultado Oeste
+        return new Oeste();
     }
 
     @Override
     public Direccion rotarDerecha() {
-        return new Este(); // Rotar 90 grados a la derecha desde el Norte da como resultado Este
+        return new Este();
     }
 
     @Override
-    public String toString() {
+    public String getNombre() {
         return "N";
     }
 }
