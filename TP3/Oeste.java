c45069f706fdcf81f95e3d002d10707e0d7aa79e
Sofia Ramos <141259824+ramossofia@users.noreply.github.com>
2024-10-25 14:09:36 -0300

    Update Oeste.java

--- TP3/Oeste.java
+++ TP3/Oeste.java
@@ -1,29 +1,28 @@
 package Explorer;
 
 public class Oeste extends Direccion {
-
     @Override
     public void moverAdelante(Explorer explorer) {
-        explorer.actualizarPosicion(explorer.getX() - 1, explorer.getY()); // Mover hacia la izquierda en el eje X
+        explorer.actualizarPosicion(explorer.getX() - 1, explorer.getY());
     }
 
     @Override
     public void moverAtras(Explorer explorer) {
-        explorer.actualizarPosicion(explorer.getX() + 1, explorer.getY()); // Mover hacia la derecha en el eje X
+        explorer.actualizarPosicion(explorer.getX() + 1, explorer.getY());
     }
 
     @Override
     public Direccion rotarIzquierda() {
-        return new Sur(); // Rotar 90 grados a la izquierda desde Oeste da como resultado Sur
+        return new Sur();
     }
 
     @Override
     public Direccion rotarDerecha() {
-        return new Norte(); // Rotar 90 grados a la derecha desde Oeste da como resultado Norte
+        return new Norte();
     }
 
     @Override
-    public String toString() {
+    public String getNombre() {
         return "O";
     }
 }
