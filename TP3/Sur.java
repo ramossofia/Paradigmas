7881fb3f77d90dea85b5027b73545a955d5ed21e
Sofia Ramos <141259824+ramossofia@users.noreply.github.com>
2024-10-28 10:29:43 -0300

    Update Sur.java

--- TP3/Sur.java
+++ TP3/Sur.java
@@ -1,6 +1,9 @@
 package Explorer;
 
 public class Sur extends Direccion {
+    private boolean escotillaSuperiorAbierta = false;
+    private boolean escotillaInferiorAbierta = false;
+
     @Override
     public void moverAdelante(Explorer explorer) {
         explorer.actualizarPosicion(explorer.getX(), explorer.getY() - 1);
@@ -24,5 +27,79 @@ public class Sur extends Direccion {
     @Override
     public String getNombre() {
         return "S";
+    }
+
+    @Override
+    public void abrirEscotillaSuperior(Explorer explorer) throws Exception {
+        validarAbrirEscotillaSuperior();
+        escotillaSuperiorAbierta = true;
+    }
+
+    @Override
+    public void abrirEscotillaInferior(Explorer explorer) throws Exception {
+        validarAbrirEscotillaInferior();
+        escotillaInferiorAbierta = true;
+    }
+
+    @Override
+    public void cerrarEscotillas(Explorer explorer) throws Exception {
+        validarCerrarEscotillas();
+        escotillaSuperiorAbierta = false;
+        escotillaInferiorAbierta = false;
+    }
+
+    @Override
+    public void aspirar(Explorer explorer) throws Exception {
+        validarAspirar();
+    }
+
+    @Override
+    public void recogerMuestra(Explorer explorer) throws Exception {
+        validarRecogerMuestra();
+    }
+
+    @Override
+    public boolean isEscotillaSuperiorAbierta() {
+        return escotillaSuperiorAbierta;
+    }
+
+    @Override
+    public boolean isEscotillaInferiorAbierta() {
+        return escotillaInferiorAbierta;
+    }
+
+    @Override
+    protected void validarAbrirEscotillaSuperior() throws Exception {
+        if (escotillaInferiorAbierta) {
+            throw new Exception("No se puede abrir escotilla superior con la inferior abierta");
+        }
+    }
+
+    @Override
+    protected void validarAbrirEscotillaInferior() throws Exception {
+        if (escotillaSuperiorAbierta) {
+            throw new Exception("No se puede abrir escotilla inferior con la superior abierta");
+        }
+    }
+
+    @Override
+    protected void validarCerrarEscotillas() throws Exception {
+        if (!escotillaSuperiorAbierta && !escotillaInferiorAbierta) {
+            throw new Exception("No hay escotillas abiertas para cerrar");
+        }
+    }
+
+    @Override
+    protected void validarAspirar() throws Exception {
+        if (!escotillaSuperiorAbierta) {
+            throw new Exception("No se puede aspirar sin abrir la escotilla superior");
+        }
+    }
+
+    @Override
+    protected void validarRecogerMuestra() throws Exception {
+        if (!escotillaInferiorAbierta) {
+            throw new Exception("No se puede recoger muestra sin abrir la escotilla inferior");
+        }
     }
 }
