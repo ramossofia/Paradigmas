75d92babe3923554082f32ce3923b454446d3103
Sofia Ramos <141259824+ramossofia@users.noreply.github.com>
2024-10-28 10:28:21 -0300

    Update Oeste.java

--- TP3/Oeste.java
+++ TP3/Oeste.java
@@ -1,6 +1,9 @@
 package Explorer;
 
 public class Oeste extends Direccion {
+    private boolean escotillaSuperiorAbierta = false;
+    private boolean escotillaInferiorAbierta = false;
+
     @Override
     public void moverAdelante(Explorer explorer) {
         explorer.actualizarPosicion(explorer.getX() - 1, explorer.getY());
@@ -24,5 +27,59 @@ public class Oeste extends Direccion {
     @Override
     public String getNombre() {
         return "O";
+    }
+
+    @Override
+    public void abrirEscotillaSuperior(Explorer explorer) throws Exception {
+        validarAbrirEscotillaSuperior();
+        escotillaSuperiorAbierta = true;
+    }
+
+    @Override
+    public void abrirEscotillaInferior(Explorer explorer) throws Exception {
+        validarAbrirEscotillaInferior();
+        escotillaInferiorAbierta = true;
+    }
+
+    @Override
+    public void cerrarEscotillas(Explorer explorer) throws Exception {
+        validarCerrarEscotillas();
+        escotillaSuperiorAbierta = false;
+        escotillaInferiorAbierta = false;
+    }
+
+    @Override
+    protected void validarAbrirEscotillaSuperior() {}
+
+    @Override
+    protected void validarAbrirEscotillaInferior() {}
+
+    @Override
+    protected void validarCerrarEscotillas() {}
+
+    @Override
+    public void aspirar(Explorer explorer) throws Exception {
+        validarAspirar();
+    }
+
+    @Override
+    protected void validarAspirar() {}
+
+    @Override
+    public void recogerMuestra(Explorer explorer) throws Exception {
+        validarRecogerMuestra();
+    }
+
+    @Override
+    protected void validarRecogerMuestra() {}
+
+    @Override
+    public boolean isEscotillaSuperiorAbierta() {
+        return escotillaSuperiorAbierta;
+    }
+
+    @Override
+    public boolean isEscotillaInferiorAbierta() {
+        return escotillaInferiorAbierta;
     }
 }
