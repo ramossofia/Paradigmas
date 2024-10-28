9e0e22df0aa0aa9b66ec1f766747831fba542c14
Sofia Ramos <141259824+ramossofia@users.noreply.github.com>
2024-10-28 10:27:39 -0300

    Update Direccion.java

--- TP3/Direccion.java
+++ TP3/Direccion.java
@@ -1,9 +1,21 @@
 package Explorer;
 
 public abstract class Direccion {
-    abstract void moverAdelante(Explorer explorer);
-    abstract void moverAtras(Explorer explorer);
-    abstract Direccion rotarIzquierda();
-    abstract Direccion rotarDerecha();
+    public abstract void moverAdelante(Explorer explorer);
+    public abstract void moverAtras(Explorer explorer);
+    public abstract Direccion rotarIzquierda();
+    public abstract Direccion rotarDerecha();
+    public abstract String getNombre();
+    public abstract void abrirEscotillaInferior(Explorer explorer) throws Exception;
+    public abstract void abrirEscotillaSuperior(Explorer explorer) throws Exception;
+    public abstract void cerrarEscotillas(Explorer explorer) throws Exception;
+    public abstract void aspirar(Explorer explorer) throws Exception;
+    public abstract void recogerMuestra(Explorer explorer) throws Exception;
+    public abstract boolean isEscotillaSuperiorAbierta();
+    public abstract boolean isEscotillaInferiorAbierta();
+    protected abstract void validarAbrirEscotillaSuperior() throws Exception;
+    protected abstract void validarAbrirEscotillaInferior() throws Exception;
+    protected abstract void validarCerrarEscotillas() throws Exception;
+    protected abstract void validarAspirar() throws Exception;
+    protected abstract void validarRecogerMuestra() throws Exception;
 }
-
