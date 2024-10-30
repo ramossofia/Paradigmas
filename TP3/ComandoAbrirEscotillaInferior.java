97fd92517ba70bead20c56b5a521656990a717f2
Sofia Ramos <141259824+ramossofia@users.noreply.github.com>
2024-10-30 20:08:59 -0300

    Update ComandoAbrirEscotillaInferior.java

--- TP3/ComandoAbrirEscotillaInferior.java
+++ TP3/ComandoAbrirEscotillaInferior.java
@@ -2,11 +2,7 @@ package Explorer;
 
 public class ComandoAbrirEscotillaInferior extends Comando {
     @Override
-    public void ejecutar(Explorer explorer) {
-        try {
-            explorer.abrirEscotillaInferior();
-        } catch (Exception e) {
-            throw new RuntimeException(e.getMessage(), e);
-        }
+    public void ejecutar(Explorer explorer)  {
+        explorer.abrirEscotillaInferior();
     }
 }
