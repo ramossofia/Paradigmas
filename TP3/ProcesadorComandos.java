1641d1f0678d4ad05bdaf9948da25fa141a4d141
Sofia Ramos <141259824+ramossofia@users.noreply.github.com>
2024-10-30 20:19:58 -0300

    Update ProcesadorComandos.java

--- TP3/ProcesadorComandos.java
+++ TP3/ProcesadorComandos.java
@@ -1,32 +1,26 @@
 package Explorer;
 
-import java.util.HashMap;
 import java.util.Map;
+import java.util.Optional;
+import java.util.stream.Stream;
 
 public class ProcesadorComandos {
 
-    private static final Map<Character, Comando> COMANDOS;
+    private final Map<Character, Comando> comandosMap = Map.of(
+            'f', new ComandoMoverAdelante(),
+            'b', new ComandoMoverAtras(),
+            'l', new ComandoRotarIzquierda(),
+            'r', new ComandoRotarDerecha(),
+            'O', new ComandoAbrirEscotillaSuperior(),
+            'o', new ComandoAbrirEscotillaInferior(),
+            'c', new ComandoCerrarEscotillas()
+    );
 
-    // Static block to initialize commands
-    static {
-        COMANDOS = new HashMap<>(Map.of(
-                'f', new ComandoMoverAdelante(),
-                'b', new ComandoMoverAtras(),
-                'l', new ComandoRotarIzquierda(),
-                'r', new ComandoRotarDerecha(),
-                'O', new ComandoAbrirEscotillaSuperior(),
-                'o', new ComandoAbrirEscotillaInferior(),
-                'c', new ComandoCerrarEscotillas()
-        ));
-
-        for (char c = 0; c < 128; c++) {
-            COMANDOS.putIfAbsent(c, new ComandoNoValido(c));
-        }
-    }
-
-    public void procesar(String comandosStr, Explorer explorer) throws Exception {
-        for (char comando : comandosStr.toCharArray()) {
-            COMANDOS.get(comando).ejecutar(explorer);
-        }
+    public void procesar(String comandosStr, Explorer explorer) {
+        Stream.of(comandosStr.split(""))
+                .map(s -> s.charAt(0))
+                .forEach(comando -> Optional.ofNullable(comandosMap.get(comando))
+                        .orElse(new ComandoNoValido(comando))
+                        .ejecutar(explorer));
     }
 }
