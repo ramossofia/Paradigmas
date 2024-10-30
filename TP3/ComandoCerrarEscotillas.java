fadcf0eeb6eaee7d81ceeed30e6f3e7eb828942a
Maria Sarachaga <141249907+mariasarachaga@users.noreply.github.com>
2024-10-30 11:42:47 -0300

    Update ComandoCerrarEscotillas.java

--- TP3/ComandoCerrarEscotillas.java
+++ TP3/ComandoCerrarEscotillas.java
@@ -3,6 +3,10 @@ package Explorer;
 public class ComandoCerrarEscotillas extends Comando {
     @Override
     public void ejecutar(Explorer explorer) {
-        explorer.cerrarEscotillas();
+        try {
+            explorer.cerrarEscotillas();
+        } catch (Exception e) {
+            System.out.println("Error: " + e.getMessage());
+        }
     }
 }
