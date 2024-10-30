e6d357915caff32bb7704572012fc2fefd860a98
Maria Sarachaga <141249907+mariasarachaga@users.noreply.github.com>
2024-10-30 13:03:25 -0300

    Update ComandoAbrirEscotillaSuperior.java

--- TP3/ComandoAbrirEscotillaSuperior.java
+++ TP3/ComandoAbrirEscotillaSuperior.java
@@ -2,12 +2,7 @@ package Explorer;
 
 public class ComandoAbrirEscotillaSuperior extends Comando {
     @Override
-    public void ejecutar(Explorer explorer) {
-        try {
-            explorer.abrirEscotillaSuperior();
-        } catch (Exception e) {
-            // Handle the exception, e.g., log it or rethrow as a runtime exception
-            System.err.println("Error opening the upper hatch: " + e.getMessage());
-        }
+    public void ejecutar(Explorer explorer) throws EscotillaException {
+        explorer.abrirEscotillaSuperior();
     }
 }
