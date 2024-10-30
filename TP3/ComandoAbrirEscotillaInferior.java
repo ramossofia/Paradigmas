e92d67159262cf872b16c46c731072ac28b5396f
Maria Sarachaga <141249907+mariasarachaga@users.noreply.github.com>
2024-10-30 17:44:05 -0300

    Update ComandoAbrirEscotillaInferior.java

--- TP3/ComandoAbrirEscotillaInferior.java
+++ TP3/ComandoAbrirEscotillaInferior.java
@@ -2,7 +2,11 @@ package Explorer;
 
 public class ComandoAbrirEscotillaInferior extends Comando {
     @Override
-    public void ejecutar(Explorer explorer) throws EscotillaException {
-        explorer.abrirEscotillaInferior();
+    public void ejecutar(Explorer explorer) {
+        try {
+            explorer.abrirEscotillaInferior();
+        } catch (Exception e) {
+            throw new RuntimeException(e.getMessage(), e);
+        }
     }
 }
