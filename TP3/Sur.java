63a6a0e915c1c535b62caac54b36bc45511df04c
Maria Sarachaga <141249907+mariasarachaga@users.noreply.github.com>
2024-10-30 21:38:23 -0300

    Update Sur.java

--- TP3/Sur.java
+++ TP3/Sur.java
@@ -1,9 +1,6 @@
 package Explorer;
 
 public class Sur extends Direccion {
-    private boolean escotillaSuperiorAbierta = false;
-    private boolean escotillaInferiorAbierta = false;
-
     @Override
     public void moverAdelante(Explorer explorer) {
         explorer.actualizarPosicion(explorer.getX(), explorer.getY() - 1);
@@ -27,64 +24,5 @@ public class Sur extends Direccion {
     @Override
     public String getNombre() {
         return "S";
-    }
-
-    @Override
-    public void abrirEscotillaSuperior(Explorer explorer) {
-        validarAbrirEscotillaSuperior();
-        escotillaSuperiorAbierta = true;
-    }
-
-    @Override
-    public void abrirEscotillaInferior(Explorer explorer) {
-        validarAbrirEscotillaInferior();
-        escotillaInferiorAbierta = true;
-    }
-
-    @Override
-    public void cerrarEscotillas(Explorer explorer) {
-        validarCerrarEscotillas();
-        escotillaSuperiorAbierta = false;
-        escotillaInferiorAbierta = false;
-    }
-
-    @Override
-    public void aspirar(Explorer explorer) {
-        validarAspirar();
-    }
-
-    @Override
-    public void recogerMuestra(Explorer explorer) {
-        validarRecogerMuestra();
-    }
-
-    @Override
-    public boolean isEscotillaSuperiorAbierta() {
-        return escotillaSuperiorAbierta;
-    }
-
-    @Override
-    public boolean isEscotillaInferiorAbierta() {
-        return escotillaInferiorAbierta;
-    }
-
-    @Override
-    protected void validarAbrirEscotillaSuperior() {
-    }
-
-    @Override
-    protected void validarAbrirEscotillaInferior() {
-    }
-
-    @Override
-    protected void validarCerrarEscotillas() {
-    }
-
-    @Override
-    protected void validarAspirar() {
-    }
-
-    @Override
-    protected void validarRecogerMuestra() {
     }
 }
