b8c7246be0e24ac683d218a43420e77781055b8c
Maria Sarachaga <141249907+mariasarachaga@users.noreply.github.com>
2024-10-30 21:35:17 -0300

    Update Direccion.java

--- TP3/Direccion.java
+++ TP3/Direccion.java
@@ -6,16 +6,4 @@ public abstract class Direccion {
     public abstract Direccion rotarIzquierda();
     public abstract Direccion rotarDerecha();
     public abstract String getNombre();
-    public abstract void abrirEscotillaInferior(Explorer explorer);
-    public abstract void abrirEscotillaSuperior(Explorer explorer);
-    public abstract void cerrarEscotillas(Explorer explorer);
-    public abstract void aspirar(Explorer explorer);
-    public abstract void recogerMuestra(Explorer explorer);
-    public abstract boolean isEscotillaSuperiorAbierta();
-    public abstract boolean isEscotillaInferiorAbierta();
-    protected abstract void validarAbrirEscotillaSuperior();
-    protected abstract void validarAbrirEscotillaInferior();
-    protected abstract void validarCerrarEscotillas();
-    protected abstract void validarAspirar();
-    protected abstract void validarRecogerMuestra();
 }
