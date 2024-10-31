052785f0b753291ae12bae8f3cd150c83a11caa2
Maria Sarachaga <141249907+mariasarachaga@users.noreply.github.com>
2024-10-30 21:37:35 -0300

    Update Norte.java

--- TP3/Norte.java
+++ TP3/Norte.java
@@ -1,19 +1,6 @@
 package Explorer;
 
 public class Norte extends Direccion {
-    private EstadoEscotilla estado;
-
-    public Norte() {
-        this.estado = new EscotillasCerradas(this);
-    }
-
-    public void setEstado(EstadoEscotilla estado) {
-        this.estado = estado;
-    }
-
-    public EstadoEscotilla getEstado() {
-        return estado;
-    }
 
     @Override
     public void moverAdelante(Explorer explorer) {
@@ -38,65 +25,5 @@ public class Norte extends Direccion {
     @Override
     public String getNombre() {
         return "N";
-    }
-
-    @Override
-    public void abrirEscotillaInferior(Explorer explorer) {
-        estado.abrirEscotillaInferior();
-    }
-
-    @Override
-    public void abrirEscotillaSuperior(Explorer explorer) {
-        estado.abrirEscotillaSuperior();
-    }
-
-    @Override
-    public void cerrarEscotillas(Explorer explorer) {
-        estado.cerrarEscotillas();
-    }
-
-    @Override
-    public void aspirar(Explorer explorer) {
-        estado.aspirar();
-    }
-
-    @Override
-    public void recogerMuestra(Explorer explorer) {
-        estado.recogerMuestra();
-    }
-
-    @Override
-    public boolean isEscotillaSuperiorAbierta() {
-        return estado instanceof EscotillaSuperiorAbierta;
-    }
-
-    @Override
-    public boolean isEscotillaInferiorAbierta() {
-        return estado instanceof EscotillaInferiorAbierta;
-    }
-
-    @Override
-    protected void validarAbrirEscotillaSuperior() {
-        // Validation logic for opening the superior hatch
-    }
-
-    @Override
-    protected void validarAbrirEscotillaInferior() {
-        // Validation logic for opening the inferior hatch
-    }
-
-    @Override
-    protected void validarCerrarEscotillas() {
-        // Validation logic for closing the hatches
-    }
-
-    @Override
-    protected void validarAspirar() {
-        // Validation logic for aspirating
-    }
-
-    @Override
-    protected void validarRecogerMuestra() {
-        // Validation logic for collecting samples
     }
 }
