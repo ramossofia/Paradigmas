9fc7df35047b6f6b5f3e1285b3f3173140f3f25a
Maria Sarachaga <141249907+mariasarachaga@users.noreply.github.com>
2024-11-01 20:13:55 -0300

    Update Comando.java

--- TP3/Comando.java
+++ TP3/Comando.java
@@ -2,4 +2,8 @@ package Explorer;
 
 public abstract class Comando {
     public abstract void ejecutar(Explorer explorer);
+
+    public boolean canHandle(char command) {
+        return false;
+    }
 }
