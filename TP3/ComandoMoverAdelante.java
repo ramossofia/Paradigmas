1bc3bf25b348c42dea9f7a50c2536f08bc749d6c
Maria Sarachaga <141249907+mariasarachaga@users.noreply.github.com>
2024-11-01 20:08:49 -0300

    Update ComandoMoverAdelante.java

--- TP3/ComandoMoverAdelante.java
+++ TP3/ComandoMoverAdelante.java
@@ -1,8 +1,13 @@
 package Explorer;
 
-public class ComandoMoverAdelante extends Comando {
+public class MoverAdelante extends Comando {
     @Override
     public void ejecutar(Explorer explorer) {
         explorer.moverAdelante();
+    }
+
+    @Override
+    public boolean canHandle(char command) {
+        return command == 'f';
     }
 }
