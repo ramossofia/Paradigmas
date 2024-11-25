989230891476709bab06369a952bc040bd9763dd
Maria Sarachaga <141249907+mariasarachaga@users.noreply.github.com>
2024-11-25 20:23:26 -0300

    Update PlaceToken.java

--- TP4/PlaceToken.java
+++ TP4/PlaceToken.java
@@ -1,22 +1,5 @@
-// src/PlaceToken.java
-public class PlaceToken implements Action {
-    private Player player;
-
+public class PlaceToken extends Action {
     public PlaceToken(Player player) {
-        this.player = player;
-    }
-
-    @Override
-    public Player getPlayer() {
-        return player;
-    }
-
-    @Override
-    public void execute(Player player, Game game) {
-        if (player.hasPlayedThisTurn()) {
-            throw new IllegalStateException(player.getName() + " has already played this turn.");
-        }
-        player.decrementTokens();  // Calls the decrementTokens method on the player
-        player.setHasPlayedThisTurn(true); // Mark this player as played
+        super(player);
     }
 }
