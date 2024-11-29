a0763361f5dcad64901144b4c00c1c738fde13e2
Maria Sarachaga <141249907+mariasarachaga@users.noreply.github.com>
2024-11-29 18:51:10 -0300

    Delete TP4/TakeCard.java

--- TP4/TakeCard.java
+++
@@ -1,9 +0,0 @@
-public class TakeCard extends Action {
-    @Override
-    public void execute(GameInProgress game, Player player) {
-        Card card = game.drawCard();
-        player.addCard(card);
-        player.addTokens(card.getTokens());
-        game.nextPlayer(); // Pasa el turno al siguiente jugador
-    }
-}
