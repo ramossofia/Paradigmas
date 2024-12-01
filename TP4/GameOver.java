d59b1ba4d5b4ef858fe6bd5e9efb75b93507b3ed
Sofia Ramos <141259824+ramossofia@users.noreply.github.com>
2024-12-01 15:00:57 -0300

    Update GameOver.java

--- TP4/GameOver.java
+++ TP4/GameOver.java
@@ -1,40 +1,23 @@
 import java.util.List;
 
-public abstract class GameStatus {
-    protected final List<Player> players;
-    protected final Deck deck;
-    protected int currentPlayerIndex;
+public class GameOver extends GameStatus {
 
-    public GameStatus(List<Player> players, Deck deck, int currentPlayerIndex) {
-        this.players = List.copyOf(players);
-        this.deck = deck;
-        this.currentPlayerIndex = currentPlayerIndex;
+    public GameOver(List<Player> players, Deck deck) {
+        super(List.copyOf(players), deck, -1);
     }
 
-    public List<Player> getPlayers() {
-        return players;
+    @Override
+    public void nextPlayer() {
     }
 
-    public Deck getDeck() {
-        return deck;
-    }
-
-    public int getCurrentPlayerIndex() {
-        return currentPlayerIndex;
-    }
-
-    public abstract void nextPlayer();
-
-    public abstract GameStatus executeAction(Action action);
-
-    protected GameStatus checkGameOver() {
-        if (deck.isEmpty()) {
-            return new GameOver(players, deck);
-        }
+    @Override
+    public GameStatus executeAction(Action action) {
         return this;
     }
 
-    public Player getCurrentPlayer() {
-        return players.get(currentPlayerIndex);
+    @Override
+    public GameStatus checkGameOver() {
+        return this;
     }
+
 }
