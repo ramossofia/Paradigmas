ac5247943c5c62e7f3a04ff2fcbe9295d6559b01
Sofia Ramos <141259824+ramossofia@users.noreply.github.com>
2024-11-30 21:32:04 -0300

    Update PlaceToken.java

--- TP4/PlaceToken.java
+++ TP4/PlaceToken.java
@@ -1,19 +1,17 @@
-public class PlaceToken implements Action {
+public class PlaceToken extends Action {
 
     @Override
     public GameStatus execute(GameStatus gameState) {
         GameInProgress game = (GameInProgress) gameState;
         Player currentPlayer = game.getCurrentPlayer();
-        Deck deck = game.getDeck(); // Assuming there's a method to get the deck
+        Deck deck = game.getDeck();
 
         if (currentPlayer.getTokens() <= 0) {
             throw new IllegalStateException("Player has no tokens to place.");
         }
 
-        // Add a token to the top card of the deck
         deck.addTokensToTopCard(1);
 
-        // Remove a token from the current player
         currentPlayer.removeToken();
 
         return game;
