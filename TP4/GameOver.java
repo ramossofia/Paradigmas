9c1a24727006bce79c51d6b8260a21c440a6fb43
Maria Sarachaga <141249907+mariasarachaga@users.noreply.github.com>
2024-12-01 16:39:22 -0300

    Update GameOver.java

--- TP4/GameOver.java
+++ TP4/GameOver.java
@@ -1,9 +1,11 @@
 import java.util.List;
 
 public class GameOver extends GameStatus {
+    private Player winner;
 
     public GameOver(List<Player> players, Deck deck) {
         super(List.copyOf(players), deck, -1);
+        this.winner = determineWinner(players);
     }
 
     @Override
@@ -20,4 +22,13 @@ public class GameOver extends GameStatus {
         return this;
     }
 
+    private Player determineWinner(List<Player> players) {
+        return players.stream()
+                .max((p1, p2) -> Integer.compare(p1.calculateScore(), p2.calculateScore()))
+                .orElseThrow(() -> new IllegalStateException("No players in the game."));
+    }
+
+    public Player getWinner() {
+        return winner;
+    }
 }
