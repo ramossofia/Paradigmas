bcf61263b556444da5b46066fc5841b92f2be87c
Sofia Ramos <141259824+ramossofia@users.noreply.github.com>
2024-12-01 15:01:58 -0300

    Update PlaceToken.java

--- TP4/PlaceToken.java
+++ TP4/PlaceToken.java
@@ -5,18 +5,16 @@ public class PlaceToken extends Action {
         GameInProgress game = (GameInProgress) gameState;
         Player currentPlayer = game.getCurrentPlayer();
 
-        // Check if the player has no tokens left, and force them to take a card
         if (currentPlayer.getTokens() <= 0) {
-            // Execute TakeCard action if the player has no tokens left
             return game.executeAction(new TakeCard());
         }
 
         if (game.hasCardsOnTable()) {
-            game.addTokensToLastTableCard(1); // Add a token to the last card on the table
+            game.addTokensToLastTableCard(1);
         }
 
-        currentPlayer.removeTokens(1); // Remove one token from the player
-        game.nextPlayer(); // Move to the next player
+        currentPlayer.removeTokens(1);
+        game.nextPlayer();
 
         return game;
     }
