3fdcc3b707c6cac6338568081afaac7c29995656
Maria Sarachaga <141249907+mariasarachaga@users.noreply.github.com>
2024-12-01 11:29:36 -0300

    Update Deck.java

--- TP4/Deck.java
+++ TP4/Deck.java
@@ -1,38 +1,18 @@
-
 import java.util.List;
 import java.util.Optional;
-import java.util.stream.Collectors;
+
 
 public class Deck {
     private List<Card> cards;
 
-    public Deck(List<Integer> cardValues) {
-        this.cards = cardValues.stream()
-                .map(value -> new Card(value, 0))
-                .collect(Collectors.toUnmodifiableList());
-    }
-
-    private Deck(List<Card> cards, boolean isPrebuilt) {
-        this.cards = List.copyOf(cards);
+    public Deck(List<Card> cards) {
+        this.cards = cards;
     }
 
     public Optional<Card> drawCard() {
         return cards.isEmpty() ? Optional.empty() : Optional.of(cards.get(0));
     }
 
-    public Deck removeTopCard() {
-        if (cards.isEmpty()) {
-            throw new IllegalStateException("The deck is empty.");
-        }
-        return new Deck(cards.subList(1, cards.size()), true);
-    }
-
-    public void addTokensToTopCard(int tokens) {
-        if (cards.isEmpty()) {
-            throw new IllegalStateException("The deck is empty.");
-        }
-        cards.get(0).addTokens(tokens);
-    }
 
     public boolean isEmpty() {
         return cards.isEmpty();
@@ -48,4 +28,9 @@ public class Deck {
                 "cards=" + cards +
                 '}';
     }
+    public void removeCard(Card card) {
+        cards.remove(card);
+    }
+
+
 }
