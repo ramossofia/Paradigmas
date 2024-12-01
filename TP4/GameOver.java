ba28083ae2a9b00f3b2172f72bcff89b2632b9f9
Maria Sarachaga <141249907+mariasarachaga@users.noreply.github.com>
2024-12-01 19:42:29 -0300

    Update GameOver.java

--- TP4/GameOver.java
+++ TP4/GameOver.java
@@ -4,22 +4,23 @@ public class GameOver extends GameStatus {
     private Player winner;
 
     public GameOver(List<Player> players, Deck deck) {
-        super(List.copyOf(players), deck, -1);
+        super(players, deck);
         this.winner = determineWinner(players);
     }
 
     @Override
     public void nextPlayer() {
+        // No-op in GameOver
     }
 
     @Override
     public GameStatus executeAction(Action action) {
-        return this;
+        return this; // No action can modify the game after it ends
     }
 
     @Override
     public GameStatus checkGameOver() {
-        return this;
+        return this; // Already in a "game over" state
     }
 
     private Player determineWinner(List<Player> players) {
