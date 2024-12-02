c2493a378dab5f851a1bf2ad053c2143659056c9
Sofia Ramos <141259824+ramossofia@users.noreply.github.com>
2024-12-01 21:32:29 -0300

    Update Card.java

--- TP4/Card.java
+++ TP4/Card.java
@@ -1,10 +1,10 @@
 public class Card implements Comparable<Card> {
     private final int value;
-    private int tokens; // Tokens associated with the card
+    private int tokens;
 
-    public Card(int value, String suit) {
+    public Card(int value) {
         this.value = value;
-        this.tokens = 0; // Initial tokens set to 0
+        this.tokens = 0;
     }
 
     public int getValue() {
@@ -15,27 +15,16 @@ public class Card implements Comparable<Card> {
         return tokens;
     }
 
-    // Method to add tokens to the card
     public void addTokens(int tokens) {
         this.tokens += tokens;
     }
 
-    // Method to remove tokens from the card
     public void removeTokens(int tokens) {
         this.tokens -= tokens;
     }
 
     @Override
     public int compareTo(Card other) {
-        return Integer.compare(this.value, other.value); // Natural order by value
+        return Integer.compare(this.value, other.value);
     }
-
-    @Override
-    public boolean equals(Object obj) {
-        if (this == obj) return true;
-        if (obj == null || getClass() != obj.getClass()) return false;
-        Card card = (Card) obj;
-        return value == card.value;
-    }
-
 }
